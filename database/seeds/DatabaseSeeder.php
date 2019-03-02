02a1485c88537ca2d152e7c7e97344bac3736234
Ahmed Tawfek <ahmedtawfek28@gmail.com>
2019-03-02 18:12:24 +0200

    new version

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
-        //$this->call(RolesTableSeeder::class);
-       // factory(App\Tag::class, 30)->create();
-        //factory(App\Category::class, 10)->create();
-       // factory(App\Post::class, 50)->create();
-       // factory(App\subscriber::class, 50)->create();
-        //factory(App\PostTag::class, 200)->create();
+        $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+       factory(App\Tag::class, 30)->create();
+        factory(App\Category::class, 10)->create();
+       factory(App\Post::class, 50)->create();
+       factory(App\subscriber::class, 50)->create();
+        factory(App\PostTag::class, 200)->create();
         factory(App\PostCategory::class, 50)->create();
  
     }
