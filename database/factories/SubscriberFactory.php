26aaf0d03c184d93b12603dd126507d9d13befff
Ahmed Tawfek <ahmedtawfek28@gmail.com>
2019-03-02 16:47:11 +0200

    make seed more professional

--- database/factories/SubscriberFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\subscriber::class, function (Faker $faker) {
-    return [
-        'email' => $faker->email
-    ];
-});
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,8 @@
 <?php
-
+use App\User;
+use App\Post;
+use App\Tag;
+use App\Category;
 use Faker\Generator as Faker;
 
 /*
@@ -12,7 +15,7 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-
+ 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
@@ -21,3 +24,47 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+$factory->define(App\Tag::class, function (Faker $faker) {
+    return [
+        'title' => $faker->word,
+        'slug' => $faker->unique()->word
+    ];
+});
+$factory->define(App\Category::class, function (Faker $faker) {
+    return [
+        'title' => $faker->word,
+        'slug' => $faker->unique()->word,
+        'image'=> $faker->image
+    ];
+});
+$factory->define(App\Post::class, function (Faker $faker) use ($factory) {
+    return [
+        'user_id' => User::all()->random()->id,
+        'title' => $faker->text(20),
+        'slug' => $faker->unique()->word,
+        'image'=> $faker->image,
+        'body'=> $faker->text,
+        'view_count'=>1924,
+        'status'=>1,
+        'is_approved'=>1
+
+    ];
+}); 
+
+$factory->define(App\subscriber::class, function (Faker $faker) {
+    return [
+        'email' => $faker->email
+    ];
+});
+$factory->define(App\PostTag::class, function (Faker $faker) use ($factory) {
+    return [
+        'post_id' => Post::all()->random()->id,
+        'tag_id' => Tag::all()->random()->id,
+    ];
+});
+$factory->define(App\PostCategory::class, function (Faker $faker) use ($factory) {
+    return [
+        'post_id' => Post::all()->random()->id,
+        'category_id' => Category::all()->random()->id,
+    ];
+}); 
